f78bd99a86f70583812901b879adce0f1d3fbdfb
ediw8311xht <maxballard333@gmail.com>
2024-05-01 03:49:16 -0400

    Z

--- handlers.py
+++ handlers.py
@@ -1,6 +1,23 @@
 #!/bin/python3
 import re
-from collections import OrderedDict
+#from collections import OrderedDict
+class Table(object):
+    table_reg = re.compile(r'[ ]*[|][ ]+(?P<table_element>[^|]+)[ ]+([|][ ]*$)?')
+
+    @classmethod
+    def table_find(cls, s):
+        match = re.finditer(cls.table_reg, s)
+        return [x.group("table_element") for x in match]
+
+    def __init__(self):
+        self.head = []
+        self.body = []
+    def as_list(self):
+        pass
+    def next_line(self):
+        pass
+        #out =  ["<th>" + x + "</th>" for x in self.head]
+        #out += ["<th>" + x + "</th>" for x in self.head]
 
 def title_handle(self, s):
     match = re.search(r'title:[ ]*(?P<title>.*)', s, re.IGNORECASE)
@@ -9,36 +26,38 @@ def title_handle(self, s):
         return ( "", "break" )
     else:
         return ( s, "continue" )
-
 def latex_handle(_self, s):
     callback = lambda x: '<span class="math inline">\(' + x.group("latex") + '\)</span>'
     match = re.sub( r'[$](?P<latex>[^$]*)[$]', callback, s)
     return ( match, "continue" )
-
-
 def bold_handle(_self, s):
     callback = lambda x: f'<b>{ x.group("bold") }</b>'
     match = re.sub( r'[*][*](?P<bold>.*?)[*][*]', callback, s)
     return ( match, "continue" )
-
 def italic_handle(_self, s):
     callback = lambda x: f'<i>{ x.group("italic") }</i>'
     match = re.sub( r'[*](?P<italic>[^*]*)[*]', callback, s)
     return ( match, "continue" )
-
+#def table_begin_handle(_self, s):
+def table_handle(_self, s):
+    match = Table.table_find(s)
+    if _self.current_table != None:
+        return Table.next(s)
+        return (s, "continue")
 def header_handle(_self, s):
     def callback(x):
         n = len(x.group("header"))
         return f'<h{ n }>{ x.group("contents") }</h{ n }>'
 
-    match = re.sub( r'^(?P<header>[#]{1,5})[ ](?P<contents>.*)$', callback, s)
-    return ( match, "continue" )
-    
-#def table_handle(_self, s):
-
+    match = re.subn( r'^(?P<header>[#]{1,5})[ ](?P<contents>.*)$', callback, s)
+    return (match[0], "break") if match[1] > 0 else (match[0], "continue")
+def paragraph_handle(_self, s):
+    return ("<p>" + s + "</p>", "break")
 
 
 if __name__ == "__main__":
-    print(latex_handle("", "help: $3+3$ out"))
-    print(italic_handle("", "**asdf**asdfadf**asdfadf**"))
+    g = "| asdfa | kdjfkj | kasdfadf |"
+    print(Table.table_find(g))
+    #print(latex_handle("", "help: $3+3$ out"))
+    #print(italic_handle("", "**asdf**asdfadf**asdfadf**"))
 
--- quick_make.py
+++ quick_make.py
@@ -1,24 +1,32 @@
 #!/bin/python3
 import re
-from collections           import OrderedDict
+#from collections           import OrderedDict
 from maceurtlib.MacHelpers import read_file, write_file
-
 import handlers as hl
 
-
 class Convert(object):
-    handle_funcs = [ hl.title_handle  , hl.latex_handle  , 
-                     hl.bold_handle   , hl.italic_handle , 
-                     hl.header_handle ,
-                   ]
+    handle_funcs = [
+        #---------------------------------Special-------------------------------#
+        hl.title_handle,        hl.latex_handle,      hl.bold_handle,   hl.italic_handle,
+        #---------------------------------Table---------------------------------#
+        #hl.table_begin_handle,  hl.table_end_handle,  hl.table_handle,
+        hl.table_handle,
+        #---------------------------------Rest----------------------------------#
+        hl.header_handle,       hl.paragraph_handle,
+    ]
     default_args = {
-        "in_file"   : "input.md", 
-        "out_file"  : "output.html", 
-        "cdn_latex" : "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js",
-        "title"     : "Notes",
-        "in_text"   : lambda s: read_file(s.in_file)
+        "in_file"       : "input.md",
+        "out_file"      : "output.html",
+        "cdn_latex"     : "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js",
+        "title"         : "Notes",
+        "in_text"       : lambda s: read_file(s.in_file),
+        "current_table" : None,
+        "structures"    : [],
+        "list_out"      : [],
     }
 
+    def __repr_(self): return "\n".join(self.list_out)
+    def __str__(self): return "\n".join(self.list_out)
     def __init__(self, **args):
         self.handle_args(args)
     def handle_args(self, d):
@@ -27,34 +35,45 @@ class Convert(object):
             if callable(set):
                 set = set(self)
             setattr(self, k, set)
+    def prep(self, l):      self.list_out = l + self.list_out
+    def appe(self, l):      self.list_out = self.list_out + l
+    def inse(self, l, p):   self.list_out = self.list_out[:p] + l + self.list_out[p + 1:]
+    def make_head(self):
+        latex = f'<script id="MathJax-script" async src="{self.cdn_latex}"></script>'
+        title = f'<title>{self.title}</title>'
+        head_l = [ "<head>", '<meta charset="utf-8"/>', title, latex, '</head>' ]
+        self.prep(head_l)
+    def make_doc(self):
+        self.prep([ '<!DOCTYPE html>', '<html>' ])
+        self.appe([ '</html>' ])
     def handle_line(self, line):
         self.state = 0
         for i in self.handle_funcs:
             line, c = i(self, line)
             if c == "break": break
-        return [line]
-    def make_head(self):
-        latex = f'<script id="MathJax-script" async src="{self.cdn_latex}"></script>'
-        title = f'<title>{self.title}</title>'
-        return [ "<head>", '<meta charset="utf-8"/>', title, latex, '</head>' ]
+        self.appe([line])
     def setup_html(self):
         il = self.in_text.split("\n")
-        ol = ['<body>']
-        #o = ["<DOCTYPE html>", ["<html>", ["<head>", "</head>"], "</html>"]]
+        self.prep(['<body>'])
         for i in il:
-            ol += self.handle_line(i)
-        return ol + ['</body>']
-    def convert_html(self):
-        l =  [ '<!DOCTYPE html>', '<html>' ]
-        l += self.make_head()
-        l += self.setup_html()
-        l += [ '</html>' ]
-        return "\n".join(l)
+            self.handle_line(i)
+        self.appe(['</body>'])
+    def convert_structures(self):
+        pass
+    def convert_html(self, l=[]):
+        self.list_out = l
+        # Parse through markdown, creating body and setting options
+        self.setup_html()
+        # Make head of list
+        self.make_head()
+        # Make document
+        self.make_doc()
+        # Convert structures [Table] in list to strings
+        self.convert_structures()
     def convert_html_file(self):
-        text_out = self.convert_html()
-        return write_file(self.out_file, text_out)
-
-
+        self.convert_html()
+        print(str(self))
+        return write_file(self.out_file, str(self))
 
 def main():
     pass
